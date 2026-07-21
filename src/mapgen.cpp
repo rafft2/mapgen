f09452c69c24c24909329af8ff994dc78b1880a7
rafft2 <rafft2-dev@pm.me>
2026-07-22 00:22:56 +0200

    fbm

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -8,6 +8,11 @@
 #include "stb_image_write.h"
 #pragma warning(pop)
 
+#define STB_PERLIN_IMPLEMENTATION
+#pragma warning(push, 0)
+#include "stb_perlin.h"
+#pragma warning(pop)
+
 #include "nom.h"
 
 enum biome_type_id : u16
@@ -49,13 +54,18 @@ struct tile_data
     f32 moisture;
 };
 
-
 #include "stdlib.h"
 #include "time.h"
 f32 RandomFloat(f32 min, f32 max)
 {
     f32 result = (f32)rand() / (f32)RAND_MAX;
     result = min + result * (max - min);
+    return(result);
+}
+
+f32 SampleNoise(f32 x, f32 y, f32 scale)
+{
+    f32 result = stb_perlin_fbm_noise3(x * scale * 5.0f, y * scale * 5.0f, 0.0f, 2.0f, 0.5f, 8);
     return(result);
 }
 
@@ -70,13 +80,13 @@ biome_type_id EvaluateBiome(f32 elevation, f32 moisture, f32 temperature)
         return(BIOME_TYPE_MOUNTAIN);
     }
     
-    if(temperature > 0.6f && moisture < 0.4f)
+    if(temperature > 0.7f && moisture < 0.4f)
     {
         return(BIOME_TYPE_DESERT);
     }
     else
     {
-        if(moisture > 0.8f)
+        if(moisture > 0.4f)
         {
             return(BIOME_TYPE_JUNGLE);
         }
@@ -93,6 +103,9 @@ int main(void)
 {
     srand((u32)time(NULL));
     s32 map_width = 256; s32 map_height = 256;
+    f32 scale = 2.5f / (f32)(map_width);
+    f32 max_elevation = 0.0f;
+    f32 max_moisture = 0.0f;
     
     tile_data* map_tile_grid = (tile_data*)malloc(sizeof(tile_data) * map_width * map_height);
     for(s32 i = 0; i < map_width; i++)
@@ -100,8 +113,19 @@ int main(void)
         for(s32 j = 0; j < map_height; j++)
         {
             map_tile_grid[IDX2D(i, j, map_width)] = {};
-            map_tile_grid[IDX2D(i, j, map_width)].elevation = RandomFloat(0.0f, 1.0f);
-            map_tile_grid[IDX2D(i, j, map_width)].moisture = RandomFloat(0.0f, 1.0f);
+            f32 elevation = fabsf(SampleNoise((f32)i, (f32)j, scale));
+            map_tile_grid[IDX2D(i, j, map_width)].elevation = elevation;
+            if(elevation > max_elevation)
+            {
+                max_elevation = elevation;
+            }
+
+            f32 moisture = fabsf(SampleNoise((f32)i + 1000.0f, (f32)j + 1000.0f, scale * 2.0f));
+            map_tile_grid[IDX2D(i, j, map_width)].moisture = moisture;
+            if(moisture > max_moisture)
+            {
+                max_moisture = moisture;
+            }
         }
     }
 
@@ -110,10 +134,10 @@ int main(void)
     {
         for(s32 j = 0; j < map_height; j++)
         {
-            f32 elevation = map_tile_grid[IDX2D(i, j, map_width)].elevation;
-            f32 moisture = map_tile_grid[IDX2D(i, j, map_width)].moisture;
+            f32 elevation = map_tile_grid[IDX2D(i, j, map_width)].elevation / max_elevation;
+            f32 moisture = map_tile_grid[IDX2D(i, j, map_width)].moisture / max_moisture;
             f32 equator = (f32)(map_height - 1) / 2.0f;
-            f32 distance_from_equator = fabsf((f32)j - equator) / 31.5f;
+            f32 distance_from_equator = fabsf((f32)j - equator) / equator;
             f32 temperature = 1.0f - ((distance_from_equator + elevation) / 2.0f);
             biome_type_id biome = EvaluateBiome(elevation, moisture, temperature);
 
